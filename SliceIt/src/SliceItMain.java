df365472b68bb956c1c58abc1d4a3bc897fec1c1
Student6 <Student6@10.8.33.108>
2017-05-04 13:06:45 -0400

    basis of the game.

--- SliceIt/src/SliceItMain.java
+++ SliceIt/src/SliceItMain.java
@@ -1,9 +1,20 @@
+import gui6.GUIApplication;
 
-public class SliceItMain {
+public class SliceItMain extends GUIApplication {
+	
+	public SliceItMain(int width, int height){
+		super(width,height);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SliceItMain slc= new SliceItMain(800,100);
+		
+	}
 
+	@Override
+	public void initScreen() {
+		SliceItScreen slice = new SliceItScreen(getWidth(),getHeight());
+		setScreen(slice);
 	}
 
 }
--- SliceIt/src/SliceItScreen.java
+++ SliceIt/src/SliceItScreen.java
@@ -1,4 +1,30 @@
+import java.util.List;
 
-public class SliceItScreen {
+import gui6.ClickableScreen;
+import gui6.components.Visible;
 
+
+
+public class SliceItScreen extends ClickableScreen implements Runnable {
+
+	public SliceItScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+	
 }
